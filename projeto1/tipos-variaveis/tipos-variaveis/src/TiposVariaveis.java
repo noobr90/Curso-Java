76574fa2f86639095197731c8bc2fb210a549d99
Bizantino <noobr90@yahoo.com.br>
2023-04-12 15:49:11 -0300

    mudança2

--- projeto1/tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ projeto1/tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -5,17 +5,23 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
         String meuNome;
+        int minhaIdade;
+        float peso;
+        String minhaIdadeConvertida;
+
         Scanner sc = new Scanner(System.in);
 
         
-        System.out.println("lets test what we learn");
-        System.out.println("digite seu nome");
-        
-        meuNome=sc.nextLine();
+        System.out.println("Digite seu nome");
+            meuNome=sc.nextLine();
+        System.out.println("Digite sua idade");
+            minhaIdade=sc.nextInt();
+        System.out.println("Digite seu peso");
+            peso=sc.nextFloat();
 
-        System.out.println(meuNome);
-
-
+        System.out.println(meuNome);           
+        System.out.println(minhaIdade);
+        System.out.println(peso);
 
 
     }
